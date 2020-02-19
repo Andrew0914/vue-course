3a757aad6f04ff4f421638109ecae57d7661841c
AndrewAlan <andrewalangm@gmail.com>
2020-02-18 21:48:46 -0600

    Installing VUE via NPM with simple webpack config

---
+++ installation/npm/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 :style="styles">{{ message }}</h1>
+  </div>
+</template>
+  
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello I'm a vue instance"
+    };
+  },
+  computed: {
+    styles() {
+      return {
+        fontFamily: "Arial",
+        color: "#4f4f4f"
+      };
+    }
+  }
+};
+</script>
