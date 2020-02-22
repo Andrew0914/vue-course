b4dec25a7308503bbccb61d0d3237f91877df5ef
AndrewAlan <andrewalangm@gmail.com>
2020-02-22 16:07:10 -0600

    Creating our first SFC : GameItem :game_die:
    - Creating the html template
    - Add some styles

--- gameshop/src/App.vue
+++ gameshop/src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <GameItem />
   </div>
 </template>
 
 <script>
-
+import GameItem from "@/components/GameItem";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components:{
+    GameItem
+  }
+};
 </script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  margin-top: 64px;
+  justify-content: center;
 }
 </style>
---
+++ gameshop/src/components/GameItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="game">
+    <a href="#" class="mb2">
+      <img
+        class="game_image"
+        src="https://gameplanet-53f8.kxcdn.com/media/catalog/product/cache/4/small_image/205x253/9df78eab33525d08d6e5fb8d27136e95/s/o/sonic-mania-plus-xbox-ps4_2.png"
+        alt="SONIC MANIA PLUS"
+      />
+    </a>
+
+    <a href="#" class="game_name mb2" title="SONIC MANIA PLUS">Sonic Mania Plus</a>
+  
+    <p class="game_price mb2">
+      <span>Nuevo</span>
+      <span>Precio: </span>
+      <span>$899</span>
+    </p>
+  
+    <div class="game_description">Sonic Mania Plus es la versión física mejorada de la versión digital original de Sonic Mania</div>
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.game {
+  max-width: 215px;
+  border: 1px solid #D3D3D3;
+  padding: 16px;
+  border-radius: 3px;
+  box-shadow: 1px 2px 3px #bdddbd;
+  text-align: center;
+}
+
+.game_image {
+  width: 200px;
+  height: 250px;
+}
+
+.game_name, .game_description, .game_price{
+  font-size: 14px;
+  color: #4f4f4f;
+}
+
+.game_name{
+  color: blueviolet;
+}
+
+.game_description{
+  font-size: 12px;
+}
+
+.game_price span:last-child{
+  font-weight: 800;
+}
+
+.mb2{
+  margin-bottom: 16px; 
+}
+</style>
