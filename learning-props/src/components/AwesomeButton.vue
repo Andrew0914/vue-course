0ca322bf3e077389e6bd210181b853b0550215fa
AndrewAlan <andrewalangm@gmail.com>
2020-03-01 12:02:43 -0600

    Creating our example component :package:

---
+++ learning-props/src/components/AwesomeButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button type="button" class="btn">
+      Awesome!
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.btn{
+    min-height: 64px;
+    border-radius: 5px;
+    border: none;
+    box-shadow: 1px 3px 4px gray;
+    cursor: pointer;
+}
+</style>
