4e3c14a34a12f58fbe85939388f778b2b5486ed4
AndrewAlan <andrewalangm@gmail.com>
2020-03-02 09:02:40 -0600

    Using class object syntax :ok_hand:

--- css-binding/src/components/CardPresentation.vue
+++ css-binding/src/components/CardPresentation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div :class="{ 'card__important': isImportant , card: !isImportant}">
       <h1>
           {{title}}
       </h1>
@@ -18,17 +18,27 @@ export default {
         description: {
             type : String,
             default: ''
+        },
+        isImportant: {
+            type: Boolean,
+            default: false
         }
     }
 }
 </script>
 
 <style scoped>
-    .card{
-        border-radius: 5px;        
+    .card, .card__important{
+        border-radius: 3px;        
         box-shadow: 1px 3px 4px grey;
         max-width: 360px;
         margin: 0 auto;
         padding: 16px;
+
+    }
+
+    .card__important{
+        border-left: 5px solid gold;
+        box-shadow: 2px 4px 5px darkgray;
     }
 </style>
