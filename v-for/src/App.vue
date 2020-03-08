5e01b53ecd468f241bc14c78f082b9733167b2fc
AndrewAlan <andrewalangm@gmail.com>
2020-03-08 14:20:10 -0600

    Simple use of v-for

--- v-for/src/App.vue
+++ v-for/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul v-for="fruit in fruits" :key="fruit">
+      <li>{{ fruit }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      fruits: ["banana", "orange", "lemon", "apple", "kiwi"]
+    }
   }
 }
 </script>
@@ -19,9 +22,6 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
