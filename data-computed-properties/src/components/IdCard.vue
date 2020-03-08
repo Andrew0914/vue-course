8cb24ac5c0fab6da34e9fece10bde9ea2f8d7264
AndrewAlan <andrewalangm@gmail.com>
2020-03-08 11:52:20 -0600

    Simple use for computed properties

--- data-computed-properties/src/components/IdCard.vue
+++ data-computed-properties/src/components/IdCard.vue
@@ -1,48 +1,51 @@
 <template>
   <div class="id__card">
-      <img :src="user.image" :alt="user.name ">
-      <div>
-          <h4>Name: {{ user.name }} </h4>
-          <p>Email: {{ user.email }} </p>
-          <p>About: {{ user.about}} </p>
-      </div>
+    <img :src="user.image" :alt="user.name " />
+    <div>
+      <h4>Name: {{ user.name }}</h4>
+      <p>Email: {{ getValidEmail }}</p>
+      <p>About: {{ user.about}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'IdCard',
-    data(){
-        return {
-            user: {
-                image: "http://codelapps.com/wp-content/uploads/2019/04/andrew.png",
-                email: "ANDREWALANGM@gmail.com",
-                name: "andrew alan gonzalez miranda",
-                about:"I am a programmer"
-            }
-        }
+  name: "IdCard",
+  data() {
+    return {
+      user: {
+        image: "http://codelapps.com/wp-content/uploads/2019/04/andrew.png",
+        email: "ANDREWALANGM@gmail.com",
+        name: "Andrew Alan Gonzalez Miranda",
+        about: "I am a programmer"
+      }
+    };
+  },
+  computed: {
+    getValidEmail() {
+      return this.user.email.toLowerCase();
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-    .id__card{
-        width: 33%;
-        border-radius: 5px;
-        background-color: #4f4f4f;
-        padding: 16px;
-        margin:0 auto;
-        display: flex;
-        color: white;
-    }
+.id__card {
+  width: 33%;
+  border-radius: 5px;
+  background-color: #4f4f4f;
+  padding: 16px;
+  margin: 0 auto;
+  display: flex;
+  color: white;
+}
 
-    .id__card img{
-        border: 1px solid gray;
-        width: 120px;
-        height: 120px;
-        margin-right: 16px;
-        border-radius: 3px;
-    }
-
-
+.id__card img {
+  border: 1px solid gray;
+  width: 120px;
+  height: 120px;
+  margin-right: 16px;
+  border-radius: 3px;
+}
 </style>
