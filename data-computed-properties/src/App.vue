07292127619bc8aaadc573eaf4c15400cbb0ff5b
AndrewAlan <andrewalangm@gmail.com>
2020-03-08 11:33:38 -0600

    Create sample project with example component :ok:
    Te component is an ID card

---
+++ data-computed-properties/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <IdCard />
+  </div>
+</template>
+
+<script>
+import IdCard from './components/IdCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    IdCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background-color: #B0B0B0;
+  padding: 16px;
+}
+</style>
---
+++ data-computed-properties/src/components/IdCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="id__card">
+      <img src="" alt="id image">
+      <div>
+          <h4>Nombre</h4>
+          <p>email</p>
+          <p>Acerca de</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .id__card{
+        width: 50%;
+        border-radius: 5px;
+        background-color: #4f4f4f;
+        padding: 16px;
+        margin:0 auto;
+        display: flex;
+        color: white;
+    }
+
+    .id__card img{
+        border: 1px solid gray;
+        width: 120px;
+        height: 120px;
+        margin-right: 16px;
+        border-radius: 3px;
+    }
+
+
+</style>
