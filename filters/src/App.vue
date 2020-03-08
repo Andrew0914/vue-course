d0af7a855f8deb9be0c41a5990562dff6d8092fc
AndrewAlan <andrewalangm@gmail.com>
2020-03-08 13:13:45 -0600

    Declaring global filter
    Create a chain of filters

--- filters/src/App.vue
+++ filters/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>
-      {{ name | uppercase }}
+      {{ name | uppercase | toFuture}}
     </h1>
   </div>
 </template>
--- filters/src/main.js
+++ filters/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.filter("toFuture", function(name){
+  return `${name} to the future `
+})
 
 new Vue({
   render: h => h(App),
