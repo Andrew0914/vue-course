8f488bd882e2c8a42a63780808fcec6ccd95196d
AndrewAlan <andrewalangm@gmail.com>
2020-03-16 14:21:09 -0600

    Using simple events

--- learning-events/src/App.vue
+++ learning-events/src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <button @click="click">
+      Click me!
+    </button>
+
+    <br>
+
+    <a href="route" @click.prevent="click">Click link!</a>
+
+    <br>
+
+    <button @mouseenter="hover = true" @mouseleave=" hover = false">
+      Hover me
+    </button>
+
+    <br>
+    
+    <div v-if="hover"> You aare over the button</div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      hover: false
+    }
+  },
+  methods:{
+    click(){
+      alert("You clicked")
+    }
   }
 }
 </script>
@@ -23,6 +46,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  max-width: 240px;
+  margin: 120px auto;
 }
 </style>
