3bcfd72037e00cf72bfdf5b9a8c8192e15e16684
AndrewAlan <andrewalangm@gmail.com>
2020-03-15 21:13:12 -0600

    Add pages views  :page_facing_up:

---
+++ router/manual/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi voluptate fugit exercitationem fugiat doloribus, nemo id natus suscipit libero eius voluptatibus consequatur inventore. Natus nobis ipsa voluptatem nesciunt, ipsum voluptas.</p>
+  </div>
+</template>
