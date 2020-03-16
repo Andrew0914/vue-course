4050b1dc4ab004efb8a3a2379bed56c924731e9a
AndrewAlan <andrewalangm@gmail.com>
2020-03-16 00:41:49 -0600

    Create nested routes :bird:

--- router/oficial/src/router.js
+++ router/oficial/src/router.js
@@ -1,15 +1,24 @@
-import VueRouter  from "vue-router";
+import VueRouter from "vue-router";
 import Home from "./views/Home";
 import About from "./views/About";
 import User from "./views/User";
+import Mision from "./views/mision";
+import Vision from "./views/vision";
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/user/:id', component: User }
-]
-
+  { path: "/", component: Home },
+  {
+    path: "/about",
+    component: About,
+    children: [
+      { path: "mision", component: Mision },
+      { path: "vision", component: Vision },
+      { path: "", component: Mision },
+    ]
+  },
+  { path: "/user/:id", component: User }
+];
 
 export const router = new VueRouter({
   routes
-})
+});
--- router/oficial/src/views/About.vue
+++ router/oficial/src/views/About.vue
@@ -2,5 +2,26 @@
   <div>
     <h1>About</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi voluptate fugit exercitationem fugiat doloribus, nemo id natus suscipit libero eius voluptatibus consequatur inventore. Natus nobis ipsa voluptatem nesciunt, ipsum voluptas.</p>
+    <TheNavbar :links="links" />
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+import TheNavbar from "../components/TheNavbar";
+
+export default {
+  name: 'About',
+  components:{
+    TheNavbar
+  },
+  data(){
+    return {
+      links: [
+        { label: "Mision", route: "/about/mision" },
+        { label: "Vision", route: "/about/vision" }
+      ]
+    }
+  }
+}
+</script>
