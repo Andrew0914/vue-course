37453b43e1e61791b4b842785c3705ba1387f679
AndrewAlan <andrewalangm@gmail.com>
2020-03-22 14:50:38 -0600

    Example component to distribute content with slots

--- slots/src/App.vue
+++ slots/src/App.vue
@@ -11,18 +11,24 @@
     <MyLink link="https://bunsan.io">
       <OtherComponent title="Welcome!" info="Go home" />
     </MyLink>
+
+    <br><br>
+
+    <Post />
   </div>
 </template>
 
 <script>
 import MyLink from './components/MyLink'
 import OtherComponent from './components/OtherComponent'
+import Post from './components/Post'
 
 export default {
   name: 'App',
   components:{
     MyLink,
-    OtherComponent
+    OtherComponent,
+    Post
   }
 }
 </script>
---
+++ slots/src/components/Post.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post">
+      <header>The header</header>
+      <main>The main content</main>
+      <footer>The footer</footer>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    .post{
+        width: 520px;
+        margin: 0 auto;
+        border: 1px solid #4f4f4f;
+        box-shadow: 2px 3px 5px #4f4f4f;
+        padding: 16px;
+        border-radius: 5px;
+    }
+</style>
