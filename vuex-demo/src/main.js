5f5cbf247af5ac954a6c3dacf5606585c6477a73
AndrewAlan <andrewalangm@gmail.com>
2020-03-29 20:26:38 -0600

    Install vuex and register store  :white_check_mark:

--- vuex-demo/src/main.js
+++ vuex-demo/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import cinemaStore from './store'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+const store = new Vuex.Store(cinemaStore)
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
