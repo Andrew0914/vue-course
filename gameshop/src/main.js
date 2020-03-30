c1b64ab8770e257051ac9a9cc157d9d514da83f3
AndrewAlan <andrewalangm@gmail.com>
2020-03-30 10:06:33 -0600

    Install and register vuex to use store :package:

--- gameshop/src/main.js
+++ gameshop/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router';
 import { router } from "./router";
+import gameStore from "./store"
+import  Vuex from "vuex";
 
 Vue.use(VueRouter);
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store(gameStore)
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
