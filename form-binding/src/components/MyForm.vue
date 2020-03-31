a2c632db4de05516b9abd39b7fba634cff864f95
AndrewAlan <andrewalangm@gmail.com>
2020-03-30 22:04:14 -0600

    Simple use of v-model to binding forms

--- form-binding/src/components/MyForm.vue
+++ form-binding/src/components/MyForm.vue
@@ -3,18 +3,18 @@
     <!-- NORMAL INPUT-->
     <div class="form-group">
       <label>Email address</label>
-      <input type="email" class="form-control"/>
+      <input type="email" class="form-control" v-model="email"/>
     </div>
 
     <!--TEXTAREA-->
     <div class="form-group">
       <label>Example textarea</label>
-      <textarea class="form-control"  rows="3"></textarea>
+      <textarea class="form-control"  rows="3" v-model="text"></textarea>
     </div>
 
     <!--CHECKBOX-->
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="defaultCheck1" />
+      <input class="form-check-input" type="checkbox" id="defaultCheck1" v-model="check"/>
       <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
     </div>
 
@@ -27,7 +27,8 @@
         type="radio"
         name="exampleRadios"
         id="exampleRadios2"
-        value="option2"
+        value="option1"
+        v-model="option"
       />
       <label class="form-check-label" for="exampleRadios2">Option 1</label>
     </div>
@@ -37,13 +38,34 @@
         type="radio"
         name="exampleRadios"
         id="exampleRadios3"
-        value="option3"
+        value="option2"
+        v-model="option"
       />
       <label class="form-check-label" for="exampleRadios3">Option 2</label>
     </div>
+
+    <br>
+    
+    <!-- SELECT -->
+    <select class="form-control" v-model="fruit">
+      <option disabled value="">Please select one</option>
+      <option value="apple">Apple</option>
+      <option value="lemon">Lemon</option>
+    </select>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  name:'MyForm',
+  data(){
+    return {
+      email:'',
+      text:'',
+      check: false,
+      option: 'option1',
+      fruit:''
+    }
+  }
+};
 </script>
