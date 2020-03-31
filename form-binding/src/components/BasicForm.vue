24aff7d87e1d78d7902ba376569019c9a42823da
AndrewAlan <andrewalangm@gmail.com>
2020-03-30 22:49:10 -0600

    Add simple validations

--- form-binding/src/components/BasicForm.vue
+++ form-binding/src/components/BasicForm.vue
@@ -2,25 +2,36 @@
   <form>
     <div class="form-group">
       <label>Name</label>
-      <input type="text" class="form-control" />
-      <small class="error">Error</small>
+      <input type="text" class="form-control" v-model="$v.name.$model"/>
+      <small class="error" v-show="$v.name.$error">Error</small>
     </div>
 
     <div class="form-group">
       <label>Age</label>
-      <input type="number" class="form-control" />
-      <small class="error">Error</small>
+      <input type="number" class="form-control"  v-model.number="$v.age.$model" />
+      <small class="error" v-show="$v.age.$error">Error</small>
     </div>
   </form>
 </template>
 
 <script>
+import { required, minLength, between } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     return {
       name: "",
-      age: 0
+      age: 20
     };
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
+    },
+    age: {
+      between: between(20, 30)
+    }
   }
 };
 </script>
