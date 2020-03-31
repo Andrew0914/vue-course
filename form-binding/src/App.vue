3e7bf942fcabc8399ea29c448ceb0b27ad069501
AndrewAlan <andrewalangm@gmail.com>
2020-03-30 21:57:28 -0600

    Create component MyForm

--- form-binding/src/App.vue
+++ form-binding/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MyForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyForm from './components/MyForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyForm
   }
 }
 </script>
@@ -21,8 +20,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 32px auto;
+  max-width: 720px;
+  background-color: lightblue;
+  padding: 16px;
+  box-sizing: border-box;
 }
 </style>
---
+++ form-binding/src/components/MyForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form>
+    <!-- NORMAL INPUT-->
+    <div class="form-group">
+      <label>Email address</label>
+      <input type="email" class="form-control"/>
+    </div>
+
+    <!--TEXTAREA-->
+    <div class="form-group">
+      <label>Example textarea</label>
+      <textarea class="form-control"  rows="3"></textarea>
+    </div>
+
+    <!--CHECKBOX-->
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="defaultCheck1" />
+      <label class="form-check-label" for="defaultCheck1">Default checkbox</label>
+    </div>
+
+    <br />
+
+    <!-- RADIOBUTTON-->
+    <div class="form-check">
+      <input
+        class="form-check-input"
+        type="radio"
+        name="exampleRadios"
+        id="exampleRadios2"
+        value="option2"
+      />
+      <label class="form-check-label" for="exampleRadios2">Option 1</label>
+    </div>
+    <div class="form-check">
+      <input
+        class="form-check-input"
+        type="radio"
+        name="exampleRadios"
+        id="exampleRadios3"
+        value="option3"
+      />
+      <label class="form-check-label" for="exampleRadios3">Option 2</label>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
