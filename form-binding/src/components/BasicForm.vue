0a0eea407e65f96e691dc53e04a3342e3977a001
AndrewAlan <andrewalangm@gmail.com>
2020-03-30 22:59:12 -0600

    Nesting data and validations :pig:

--- form-binding/src/components/BasicForm.vue
+++ form-binding/src/components/BasicForm.vue
@@ -1,15 +1,15 @@
 <template>
   <form @submit.prevent="save">
-    <div class="form-group" >
+    <div class="form-group">
       <label>Name</label>
-      <input type="text" class="form-control" v-model="$v.name.$model" />
-      <small class="error" v-show="$v.name.$error">Error</small>
+      <input type="text" class="form-control" v-model="$v.form.name.$model" />
+      <small class="error" v-show="$v.form.name.$error">Error</small>
     </div>
 
     <div class="form-group">
       <label>Age</label>
-      <input type="number" class="form-control" v-model.number="$v.age.$model" />
-      <small class="error" v-show="$v.age.$error">Error</small>
+      <input type="number" class="form-control" v-model.number="$v.form.age.$model" />
+      <small class="error" v-show="$v.form.age.$error">Error</small>
     </div>
 
     <button type="submit" class="btn btn-primary">Submit</button>
@@ -22,27 +22,32 @@ import { required, minLength, between } from "vuelidate/lib/validators";
 export default {
   data() {
     return {
-      name: "",
-      age: 20
+      form: {
+        name: "",
+        age: 20
+      }
     };
   },
   validations: {
-    name: {
-      required,
-      minLength: minLength(4)
-    },
-    age: {
-      between: between(20, 30)
+    form: {
+      name: {
+        required,
+        minLength: minLength(4)
+      },
+      age: {
+        between: between(20, 30)
+      }
     }
   },
   methods: {
     save() {
-        this.$v.$touch()
-        if(this.$v.$invalid){
-            alert('The form is wrong')
-        } else {
-            alert('Form was submitterd successfully')
-        }
+      this.$v.form.$touch();
+      if (this.$v.form.$invalid) {
+        alert("The form is wrong");
+      } else {
+        alert("Form was submitterd successfully");
+        console.log({...this.form});
+      }
     }
   }
 };
