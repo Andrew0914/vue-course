1f61fefe3390f7442d3bd94359c1e607b6af6549
AndrewAlan <andrewalangm@gmail.com>
2020-03-30 22:55:39 -0600

    Submit the form after validate

--- form-binding/src/components/BasicForm.vue
+++ form-binding/src/components/BasicForm.vue
@@ -1,21 +1,23 @@
 <template>
-  <form>
-    <div class="form-group">
+  <form @submit.prevent="save">
+    <div class="form-group" >
       <label>Name</label>
-      <input type="text" class="form-control" v-model="$v.name.$model"/>
+      <input type="text" class="form-control" v-model="$v.name.$model" />
       <small class="error" v-show="$v.name.$error">Error</small>
     </div>
 
     <div class="form-group">
       <label>Age</label>
-      <input type="number" class="form-control"  v-model.number="$v.age.$model" />
+      <input type="number" class="form-control" v-model.number="$v.age.$model" />
       <small class="error" v-show="$v.age.$error">Error</small>
     </div>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
   </form>
 </template>
 
 <script>
-import { required, minLength, between } from 'vuelidate/lib/validators'
+import { required, minLength, between } from "vuelidate/lib/validators";
 
 export default {
   data() {
@@ -31,6 +33,16 @@ export default {
     },
     age: {
       between: between(20, 30)
+    }
+  },
+  methods: {
+    save() {
+        this.$v.$touch()
+        if(this.$v.$invalid){
+            alert('The form is wrong')
+        } else {
+            alert('Form was submitterd successfully')
+        }
     }
   }
 };
