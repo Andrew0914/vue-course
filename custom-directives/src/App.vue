7f7ceb74727187993d03a4962c97fb5ca27968cd
AndrewAlan <andrewalangm@gmail.com>
2020-04-05 18:45:17 -0500

    Simple v-focus directive

--- custom-directives/src/App.vue
+++ custom-directives/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <input type="text" placeholder="Input 1" />
+    <br />
+    <br />
+    <input type="text" placeholder="Input 2" v-focus />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  directives: {
+    focus: {
+      // directive definition
+      inserted: function(el) {
+        el.focus();
+      }
+    }
   }
 }
 </script>
@@ -24,5 +30,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  min-height: 32px;
+  min-width: 214px;
 }
 </style>
