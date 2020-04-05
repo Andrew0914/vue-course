1d28efeebc9a4af8f36ceb8ad2a8fcd64a090f29
AndrewAlan <andrewalangm@gmail.com>
2020-04-05 18:48:54 -0500

    Adding arguments

--- custom-directives/src/directives/party.js
+++ custom-directives/src/directives/party.js
@@ -4,11 +4,22 @@ export default {
         const colors = binding.value;
         let i = 0;
 
+        // arguments
+        const speeds = {
+            "slow": 2000,
+            "normal": 1000,
+            "fast": 500,
+            "crazy": 100
+        };
+
+        const speedName = binding.arg || 'normal';
+        const speed = speeds[speedName];
+
         // podemos guardar la referencia para usarla en unbind
         el.__ColorInterval__ = setInterval(() => {
             el.style.color = colors[i++];
             if (i > colors.length - 1) i = 0;
-        }, 1000);
+        }, speed);
     },
     unbind(el) {
         clearInterval(el.__ColorInterval__);
