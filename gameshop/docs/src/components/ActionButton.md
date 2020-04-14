088715892a9067f6ccb5da1220581aac94cc2bd3
AndrewAlan <andrewalangm@gmail.com>
2020-04-13 21:22:39 -0500

    Install vue-docgen and create some documentation :paperclip:

---
+++ gameshop/docs/src/components/ActionButton.md
@@ -0,0 +1,22 @@
+# ActionButton
+
+> A button to general purpouse
+
+## Props
+
+| Prop name | Description                  | Type   | Values | Default |
+| --------- | ---------------------------- | ------ | ------ | ------- |
+| event     | The event name to be emitted | string | -      | "click" |
+
+## Events
+
+| Event name  | Type | Description   |
+| ----------- | ---- | ------------- |
+| custom name |      | Custom event. |
+
+## Slots
+
+| Name    | Description                           | Bindings |
+| ------- | ------------------------------------- | -------- |
+| default | Use this slot to display label button |          |
+| icon    | Use this slot to set icon button      |          |
--- gameshop/src/components/ActionButton.vue
+++ gameshop/src/components/ActionButton.vue
@@ -1,43 +1,62 @@
 <template>
-  <button class="action" @click="$emit(event)">
+  <button class="action" @click="emit">
+    <!-- @slot Use this slot to display label button -->
     <slot />
-    <slot name="icon"/>
+    <!-- @slot Use this slot to set icon button -->
+    <slot name="icon" />
   </button>
 </template>
 
 <script>
+/**
+ * A button to general purpouse
+ * @displayName ActionButton
+ */
 export default {
   name: "ActionButton",
   props: {
+    /**
+     * The event name to be emitted
+     */
     event: {
       type: String,
-      default: 'click'
+      default: "click"
+    }
+  },
+  methods: {
+    emit() {
+      /**
+       * Custom event.
+       *
+       * @event custom name
+       */
+      this.$emit(this.event);
     }
   }
 };
 </script>
 
 <style scoped>
-.action{
-    padding: 8px;
-    border-radius: 3px;
-    background-color: rgb(251,188,4);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    min-width: 100px;
-    margin: 8px 0;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    font-size: 16px;
+.action {
+  padding: 8px;
+  border-radius: 3px;
+  background-color: rgb(251, 188, 4);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 100px;
+  margin: 8px 0;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  font-size: 16px;
 }
 
-.action img{
-    margin: auto 0;
-    width: 16px;
-    height: 16px;
-    max-height: 16px;
-    max-width: 16px;
+.action img {
+  margin: auto 0;
+  width: 16px;
+  height: 16px;
+  max-height: 16px;
+  max-width: 16px;
 }
 </style>
